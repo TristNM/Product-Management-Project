9513fcbadc1db08ee4c4bbfbfa45a2fb7db04962
MTriet2301 <trietnguyen1892@gmail.com>
2024-07-07 16:23:20 +0700

    update products database

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,13 @@
-module.exports.products= (req, res) => {
+// [GET] /admin/products
+
+const Product = require("../../model/product.model");
+module.exports.products= async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
     res.render("admin/pages/products/index.pug", {
-        title: "Products"
+        title: "Products",
+        products: products
     });
 }
 
--- controllers/clients/home..controllers.js
+++ controllers/clients/home..controllers.js
@@ -1,3 +1,4 @@
+// [GET] /products
 module.exports.home = (req, res) => {
     res.render("client/pages/home/index", {
         title: "Home"
